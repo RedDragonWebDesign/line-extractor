aabdcc03453ab52d5b36d678641614e5df045fc3
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-06-29 11:22:44 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="style.css" />
+	<script src="line-extractor.js"></script>
+</head>
+
+<body>
+	<h1>
+	Line Extractor
+	</h1>
+	
+	<p>
+	A tool/utility where you search for keywords, and if a line contains that keyword, it will move the lines to their own file.
+	</p>
+	
+	<p>
+	Search Words (One Per Line):<br />
+	<textarea id="needle">cookie
+gdpr</textarea><br />
+	</p>
+	
+	<div id="container">
+		<p>
+		File:<br >
+		<textarea id="haystack">! AdBlock filters
+! moving "cookie" and "gdpr" filters to their own file example
+! Start Web Annoyances Ultralist - Generic Filters
+###BackToTop
+###BacktoTop
+###Banner_cookie_1
+###CookieNotification
+###CybotCookiebotDialog
+###GDPRAlertBanner
+###GDPRConsentBar
+###GDPRCookie
+###GoUpWrapper
+###Hero-0-FeatureBar-smartphone-Proxy
+###PopupSignupForm_0
+###ShareToolsMobile
+###SocialMediaIconsID
+###TopShareMenu
+###WNStoryUtils
+###___plusone_0
+###___plusone_1
+###___ratingbadge_0
+###__tealiumGDPRecModal
+###__tealiumImplicitmodal
+###_evidon_banner
+###actionbar
+###addthis0
+###addthisSticky
+###addthis_widget
+###adhesionUnit
+###adv_mobi_edition
+###adv_network
+###adviceCookies
+###alert_cookies
+###amp-cookie-notice
+###appIntercept
+###apply-privacy-policy
+###article-share</textarea>
+		</p>
+		
+		<p>
+		Output:<br />
+		<textarea id="output"></textarea>
+		</p>
+	</div>
+	
+	<p>
+	<button id="extract-lines">Extract Lines</button>
+	</p>
+	
+	<p>
+	Want to report a bug or request a feature? <a href="https://github.com/GeneralKenobi1/line-extractor/issues">Create an issue</a> on our GitHub.
+	</p>
+</body>
+
+</html>
---
+++ line-extractor.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class LineExtractor {
+	needle = "";
+	haystack = "";
+	newHaystack = "";
+	output = "";
+	
+	constructor(needle, haystack) {
+		this.needle = needle;
+		this.haystack = haystack;
+		
+		if ( ! needle || ! haystack ) {
+			newHaystack = haystack;
+			return;
+		}
+		
+		// figure out regex pattern
+		let needleArray = needle.split("\n");
+		let regExPattern = "";
+		for ( let line of needleArray) {
+			regExPattern += Helper.sanitizeRegEx(line) + "|";
+		}
+		regExPattern = Helper.chopRight(regExPattern, 1);
+		regExPattern = new RegExp(regExPattern, 'i');
+		console.log(regExPattern);
+		
+		// extract lines
+		let haystackArray = haystack.split("\n");
+		let newHaystack = "";
+		let output = "";
+		for ( let line of haystackArray ) {
+			if ( line.search(regExPattern) !== -1 ) {
+				output += line + "\n";
+			} else {
+				newHaystack += line + "\n";
+			}
+		}
+		
+		this.newHaystack = Helper.chopRight(newHaystack, 1);
+		this.output = Helper.chopRight(output, 1);
+	}
+}
+
+class Helper {
+	static chopRight(string, length) {
+		return string.slice(0, string.length - length);
+	}
+	
+	/** Escapes RegEx special chars so that it can be included in a RegEx pattern without side effects -[]{}()*+?.,\^$|# whitespace */
+	static sanitizeRegEx(text) {
+		return text.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&');
+	}
+}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+	let extractLines = document.getElementById('extract-lines');
+	let needle = document.getElementById('needle');
+	let haystack = document.getElementById('haystack');
+	let output = document.getElementById('output');
+	
+	extractLines.addEventListener('click', function(e) {
+		let lineExtractor = new LineExtractor(needle.value, haystack.value);
+		output.value = lineExtractor.output;
+		haystack.value = lineExtractor.newHaystack;
+	});
+});
